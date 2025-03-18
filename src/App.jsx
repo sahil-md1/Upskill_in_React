060b004501e877b7f1977ff3fe754ee854319305
md sahil <ms.mdsahil77@gmail.com>
2025-03-19 04:08:22 +0530

    tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import User from "./User";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>
+    <div className="p-4 bg-blue-500">
       React
+      <User />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/User.jsx
@@ -0,0 +1,18 @@
+// export default function User() {
+//   return <div>User Check</div>;
+// }
+// export default User;
+
+
+import React from 'react'
+
+const User = () => {
+  return (
+    <div className=''>
+      
+    </div>
+  )
+}
+
+export default User
+
