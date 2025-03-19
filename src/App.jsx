3382a6605cb421695f8c516fe5509cc8e4fe3e3e
md sahil <ms.mdsahil77@gmail.com>
2025-03-20 00:07:04 +0530

    app.js file

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import User from "./User";
+import User from "./Components/User";
+import State from "./Components/state";
 function App() {
   return (
     <div className="p-4 bg-blue-500">
       React
       <User />
+      <State/>
     </div>
   );
 }
