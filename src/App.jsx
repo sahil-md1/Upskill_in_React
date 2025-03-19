4ba86fc35b7d92b502c81f98843739b0c18029f1
md sahil <ms.mdsahil77@gmail.com>
2025-03-20 00:46:22 +0530

    class component

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import User from "./Components/User";
 import State from "./Components/state";
+import ClassCompt from "./Components/ClassCompt";
 function App() {
   return (
     <div className="p-4 bg-blue-500">
       React
       <User />
       <State/>
+      {/* <ClassCompt/> */}
     </div>
   );
 }
---
+++ src/Components/ClassCompt.tsx
@@ -0,0 +1,17 @@
+// import React, { Component } from "react";
+
+// class ClassCompt extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       message: "anil",
+//     };
+//   }
+//   render() {
+//     return <div>
+//         <h1>{this.state.message}</h1>
+//     </div>;
+//   }
+// }
+
+// export default ClassCompt;
